89dd910efd7dcb3c3a13a68d99457ad51bc5e517
antoniocostantin <costantini062@gmail.com>
2024-10-31 17:53:28 +0100

    init

---
+++ src/main/java/antonio/costantini/progettosettimale6/Progettosettimale6Application.java
@@ -0,0 +1,13 @@
+package antonio.costantini.progettosettimale6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Progettosettimale6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Progettosettimale6Application.class, args);
+	}
+
+}
