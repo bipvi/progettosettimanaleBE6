189e2cd8158c507189dcef3d4530dc8d00bb0a3d
antoniocostantin <costantini062@gmail.com>
2024-11-03 19:20:25 +0100

    funge tutto

--- src/main/java/antonio/costantini/progettosettimale6/Progettosettimale6Application.java
+++ src/main/java/antonio/costantini/progettosettimale6/Progettosettimale6Application.java
@@ -3,6 +3,10 @@ package antonio.costantini.progettosettimale6;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+// non sarà il progetto più completo di sempre ma dovevo recuperare e sono molto soddisfatto
+
+
 @SpringBootApplication
 public class Progettosettimale6Application {
 
